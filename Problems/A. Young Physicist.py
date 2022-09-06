218766e3955088364546db26d09c984c2ef65c2b
Eslam21 <Es.Ahmed@nu.edu.eg>
2022-09-06 22:35:48 +0200

    new problem

--- Problems/A. Young Physicist.py
+++ Problems/A. Young Physicist.py
@@ -1,11 +1,6 @@
-# to take input list 
-# arr=list(map(int,input().split()))
-
-
-
                                 ##### The Approach #####
                                 
-
+#Sum the x's,y's and z's. Check if all of them equal 0
 
 #The Code#   
 
@@ -14,5 +9,5 @@ x,y,z=0,0,0
 for i in range(0,n):
     arr=list(map(int,input().split()))
     x,y,z=arr[0]+x, arr[1]+y, arr[2]+z 
-print('NYOE S'[x==0 and y==0 and z==0::2])  
-print(['NO','YES'][x==y==z==0])
+print('NYOE S'[x==0 and y==0 and z==0::2])  # returns either True:0 or Flase:1 
+print(['NO','YES'][x==y==z==0])  # returns either True:0 or Flase:1 
