f4490660a63e74862cf0f63a4d570d367a81e4bb
Eslam21 <Es.Ahmed@nu.edu.eg>
2022-11-26 10:17:43 +0200

    Update Coding.py

--- Coding.py
+++ Coding.py
@@ -6,36 +6,8 @@
 
 ##################################################################################################
 #The Code#   
-import itertools
-def check_lucky(x):
-    x=str(x)
-    lucky=True
-    for i in x:
-        if i != '4' and i != '7':
-            lucky=False
-            break
-            
-        else:
-         continue
-    
-    return lucky
- 
-def generate_lucky():
-   lucky=[]
- 
-   for x in range(1,4):
-    for i in itertools.product(['4', '7'],repeat=x):
-        lucky.append(int(''.join(i)))
-   return lucky
- 
-num=int(input())
- 
-if check_lucky(num)==True:
-    print('YES')
-else:
-    for i in generate_lucky():
-        if num%i==0:
-            print("YES")
-            break
-    else:
-        print("NO")  
+num = int(input())
+magnet= [input() for i in range(num)]
+ans=[ 1  for i in range(0,num-1) if magnet[i][1] != magnet[i+1][1]  ]
+
+print(len(ans)+1)        
